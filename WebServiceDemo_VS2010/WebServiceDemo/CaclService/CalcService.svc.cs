8af4d3130f78b8c355ed48ef386883a8ca0e49de
Jim Paterson <James.Paterson@gcu.ac.uk>
2013-05-26 20:15:17 +0100

    updated slides and added VS2010 version

---
+++ WebServiceDemo_VS2010/WebServiceDemo/CaclService/CalcService.svc.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CaclService
+{
+    public class CalcService : ICalcService
+    {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Subtract(double x, double y)
+        {
+            return x - y;
+        }
+
+        public double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+
+        public double Divide(double x, double y)
+        {
+            return x / y;
+        }
+    }
+}
---
+++ WebServiceDemo_VS2010/WebServiceDemo/CaclService/ICalcService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CaclService
+{
+    [ServiceContract]
+    public interface ICalcService
+    {
+        [OperationContract]
+        double Add(double x, double y);
+
+        [OperationContract]
+        double Subtract(double x, double y);
+
+        [OperationContract]
+        double Multiply(double x, double y);
+
+        [OperationContract]
+        double Divide(double x, double y);
+    }
+}
